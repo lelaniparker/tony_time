71165d88d940449e6b74c99d3678b625619479cb
BlueCodeThree <carliemhamilton@gmail.com>
2019-09-24 22:13:30 +1000

    adds searching for a cocktail on the homepage

--- cocktail_by_name.js
+++ cocktail_by_name.js
@@ -19,19 +19,23 @@ function showCocktail(data) {
     let drink = data.drinks[0];
     console.group(drink);
     // // create div card to display drink:
+    const drinkImg = document.createElement('div');
+    drinkImg.className = "drinkImg";
+    cocktail.appendChild(drinkImg);
     const card = document.createElement('div');
     card.className = "card";
     cocktail.appendChild(card);
-    // we can add in an image - the url is drink.strDrinkThumb
-    cocktail.innerHTML = "Name: " + drink.strDrink + "<br />";
-    cocktail.innerHTML += "Ingredients: <br />"
-    // Display as html list using string interpolation
-    cocktail.innerHTML += drink.strMeasure1 + drink.strIngredient1 + "<br />";
-    cocktail.innerHTML += drink.strMeasure2 + drink.strIngredient2 + "<br />";
-    cocktail.innerHTML += drink.strMeasure3 + drink.strIngredient3 + "<br />";
-    cocktail.innerHTML += drink.strMeasure4 + drink.strIngredient4 + "<br />";
-    cocktail.innerHTML += drink.strMeasure5 + drink.strIngredient5 + "<br /><br />";
-    cocktail.innerHTML += "Instructions: <br />" + drink.strInstructions;
+
+    drinkImg.innerHTML = "<img src='" + drink.strDrinkThumb + "' style='margin: 0px;'> ";
+    card.innerHTML += "<b>Name:</b> ";
+    card.innerHTML += drink.strDrink + "<br /><br />";
+    card.innerHTML += "<b>Ingredients:</b> <br />";
+    card.innerHTML += drink.strMeasure1 + drink.strIngredient1 + "<br />";
+    card.innerHTML += drink.strMeasure2 + drink.strIngredient2 + "<br />";
+    card.innerHTML += drink.strMeasure3 + drink.strIngredient3 + "<br />";
+    card.innerHTML += drink.strMeasure4 + drink.strIngredient4 + "<br />";
+    card.innerHTML += drink.strMeasure5 + drink.strIngredient5 + "<br /><br />";
+    card.innerHTML += "<b>Instructions: </b><br />" + drink.strInstructions ;
 }
 
 // searchCocktail function
@@ -40,4 +44,9 @@ submitButton.addEventListener('click', (e) => {
     let searchValue = document.getElementById('searchText').value;
     url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + searchValue;
     getJSON();
+    clearSearchValue();
 });
+
+function clearSearchValue(){
+    document.getElementById("searchText").value="";
+}
