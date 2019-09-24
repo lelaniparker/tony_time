65f552ea2e0efc66d467e7df33aaedf4e61635b5
BlueCodeThree <carliemhamilton@gmail.com>
2019-09-24 22:51:05 +1000

    wip - adds what i've done sofar for display all
    I would like to have some kind of selection so you can choose a letter

--- scripts/all.js
+++ scripts/all.js
@@ -1,7 +1,7 @@
-let url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+let url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
 const cocktail = document.getElementById('cocktail');
 const searchForm = document.querySelector('.form');
-const submitButton = document.getElementById('submit');
+const cocktailInfo = document.getElementById('cocktailInfo');
 
 // asynchronous function to get the JSON document from the url
 function getJSON() {
@@ -12,32 +12,25 @@ function getJSON() {
             console.error(e);
         });
 }
-
+getJSON();
 // function to display data in browser
 // WE COULD PUT THIS REPEATED CODE IN A SEPARATE FILE
 function showCocktail(data) {
-    let drink = data.drinks[0];
-    console.group(drink);
-    // // create div card to display drink:
     const card = document.createElement('div');
     card.className = "card";
     cocktail.appendChild(card);
-    // we can add in an image - the url is drink.strDrinkThumb
-    cocktail.innerHTML = "Name: " + drink.strDrink + "<br />";
-    cocktail.innerHTML += "Ingredients: <br />"
-    // Display as html list using string interpolation
-    cocktail.innerHTML += drink.strMeasure1 + drink.strIngredient1 + "<br />";
-    cocktail.innerHTML += drink.strMeasure2 + drink.strIngredient2 + "<br />";
-    cocktail.innerHTML += drink.strMeasure3 + drink.strIngredient3 + "<br />";
-    cocktail.innerHTML += drink.strMeasure4 + drink.strIngredient4 + "<br />";
-    cocktail.innerHTML += drink.strMeasure5 + drink.strIngredient5 + "<br /><br />";
-    cocktail.innerHTML += "Instructions: <br />" + drink.strInstructions;
+    cocktail.innerHTML += "<ul>"
+    for (let drink of data.drinks) {
+        console.group(drink.strDrink);
+        cocktail.innerHTML += "<li>" + drink.strDrink + "</li>";
+    }
+    cocktail.innerHTML += "</ul>"
 }
 
 // searchCocktail function
-submitButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    let searchValue = document.getElementById('searchText').value;
-    url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + searchValue;
-    getJSON();
-});
+// cocktailInfo.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     let searchValue = document.getElementById('searchText').value;
+//     url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + searchValue;
+//     getJSON();
+// });
