65f817084497516d59841fc5d6d556ac63fc17c4
BlueCodeThree <carliemhamilton@gmail.com>
2019-09-25 13:58:07 +1000

    eeuoe

--- scripts/all.js
+++ scripts/all.js
@@ -32,110 +32,132 @@ alphabet.addEventListener('change', (e) => {
     e.preventDefault();
     switch(alphabet.value) {
         case "A":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "a";
             getJSON()
             break;
         case "B":
-            url = url + "b";
-            getJSON()
-            break;
-        case "B":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "b";
             getJSON()
             break;
         case "C":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "c";
             getJSON()
             break;
                     case "D":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "d";
             getJSON()
             break;
                     case "E":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "e";
             getJSON()
             break;
                     case "F":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "f";
             getJSON()
             break;
                     case "G":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "g";
             getJSON()
             break;
                     case "H":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "h";
             getJSON()
             break;
                     case "I":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "i";
             getJSON()
             break;
                     case "J":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "j";
             getJSON()
             break;
                     case "K":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "k";
             getJSON()
             break;
                     case "L":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "l";
             getJSON()
             break;
                     case "M":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "m";
             getJSON()
             break;
                     case "N":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "n";
             getJSON()
             break;
                     case "O":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "o";
             getJSON()
             break;
                     case "P":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "p";
             getJSON()
             break;
                     case "Q":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "q";
             getJSON()
             break;
                     case "R":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "r";
             getJSON()
             break;
                     case "S":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "s";
             getJSON()
             break;
                     case "T":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "t";
             getJSON()
             break;
                     case "U":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "u";
             getJSON()
             break;
                     case "V":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "v";
             getJSON()
             break;
                     case "W":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "w";
             getJSON()
             break;
                     case "X":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "x";
             getJSON()
             break;
                     case "Y":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "y";
             getJSON()
             break;
                     case "Z":
+            url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=";
             url = url + "z";
             getJSON()
             break;
